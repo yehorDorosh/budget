de21843f4794ec32298529c2d4b43eead08d0e78
Egor Dorosh <egor.dorosh@libertexgroup.com>
2023-08-31 12:53:56 +0200

    BE: Add routes for category

--- server/src/routes/categories.ts
+++ server/src/routes/categories.ts
@@ -2,12 +2,18 @@ import express from 'express'
 
 import { notEmptyValidator } from '../utils/validators'
 
-import { addCategory } from '../controllers/categories'
+import { addCategory, updateCategory, deleteCategory, getCategories } from '../controllers/categories'
 import auth from '../middleware/auth'
 import { validationErrorsHandler } from '../utils/errors'
 
 const router = express.Router()
 
-router.post('/add', notEmptyValidator('name'), validationErrorsHandler('Categories alidation failed'), auth, addCategory)
+router.post('/add', notEmptyValidator('name'), validationErrorsHandler('Categories validation failed'), auth, addCategory)
+
+router.get('/get', auth, getCategories)
+
+router.delete('/delete', auth, deleteCategory)
+
+router.put('/update', notEmptyValidator('name'), validationErrorsHandler('Categories validation failed'), auth, updateCategory)
 
 export default router
