ff8e1aa1f3310c94f35feaf001273a22190f58a3
Egor Dorosh <egor.dorosh@libertexgroup.com>
2023-08-31 12:40:18 +0200

    Fix not uniq category bug

--- server/src/controllers/categories.ts
+++ server/src/controllers/categories.ts
@@ -3,6 +3,7 @@ import { AppRes, ResCodes } from '../types/express/custom-response'
 
 import { errorHandler } from '../utils/errors'
 import { CategoryCRUD } from '../db/crud'
+import { QueryFailedError } from 'typeorm'
 
 export const addCategory: RequestHandler = async (req, res: AppRes<CategoriesPayload>, next) => {
   const user = req.user!
@@ -15,8 +16,23 @@ export const addCategory: RequestHandler = async (req, res: AppRes<CategoriesPay
 
     res.status(201).json({ message: 'Create new category', code: ResCodes.CREATE_CATEGORY, payload: { categories: categories || [] } })
   } catch (err) {
-    console.log(err)
-    errorHandler({ message: 'Failed to create new category', details: err }, next)
+    if (err instanceof QueryFailedError && err.driverError.code == 23505) {
+      res.status(422).json({
+        message: 'Category already exist',
+        code: ResCodes.VALIDATION_ERROR,
+        validationErrors: [
+          {
+            location: 'body',
+            msg: 'Category already exist',
+            path: 'name',
+            type: 'field',
+            value: name
+          }
+        ]
+      })
+    } else {
+      errorHandler({ message: 'Failed to create new category', details: err }, next)
+    }
   }
 }
 
@@ -29,6 +45,26 @@ export const getCategories: RequestHandler = async (req, res: AppRes<CategoriesP
     res
       .status(200)
       .json({ message: 'Category list successfully provided', code: ResCodes.GET_CATEGORIES, payload: { categories: categories || [] } })
+  } catch (err) {
+    console.log(err)
+    errorHandler({ message: 'Failed to create new user', details: err }, next)
+  }
+}
+
+export const deleteCategory: RequestHandler = async (req, res: AppRes<CategoriesPayload>, next) => {
+  const categoryId = req.query.id ? +req.query.id : null
+  if (!categoryId) {
+    return errorHandler({ message: 'deleteCategory. Invalid query param id', statusCode: 500 }, next)
+  }
+
+  try {
+    const category = await CategoryCRUD.delete(categoryId, next)
+
+    res.status(200).json({
+      message: 'Category list successfully provided',
+      code: ResCodes.GET_CATEGORIES,
+      payload: { categories: category ? [category] : [] }
+    })
   } catch (err) {
     console.log(err)
     errorHandler({ message: 'Failed to create new user', details: err }, next)
