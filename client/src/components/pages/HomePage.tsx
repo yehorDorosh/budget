2b6811c9a51318a5caa3dfe413e17cecce57dc5a
Egor Dorosh <egor.dorosh@libertexgroup.com>
2023-09-13 10:17:02 +0200

    Fix categories list for main page

--- client/src/components/pages/HomePage.tsx
+++ client/src/components/pages/HomePage.tsx
@@ -1,10 +1,18 @@
-import { Fragment } from 'react'
-import { useAppSelector } from '../../hooks/useReduxTS'
+import { Fragment, useEffect } from 'react'
+import { useAppSelector, useAppDispatch } from '../../hooks/useReduxTS'
 import AddBudgetItemForm from '../budget/AddBudgetItemForm/AddBudgetItemForm'
+import { getCategories } from '../../store/categories/categories-actions'
 
 const HomePage = () => {
+  const dispatch = useAppDispatch()
   const isLogin = useAppSelector((state) => state.user.isLogin)
   const token = useAppSelector((state) => state.user.token)
+
+  useEffect(() => {
+    if (token) {
+      dispatch(getCategories({ token }))
+    }
+  }, [token, dispatch])
 
   return (
     <Fragment>
